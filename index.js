e9e67819a3808f04c024a913c2862a68a040c69d
karanagarwal12 <karanagarwal200505@gmail.com>
2024-03-15 02:11:44 +0530

    added game over on self collision,issue #4

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ function isCollide(snake) {
     if (snake[0].x > 18 || snake[0].x < 0 || snake[0].y > 18 || snake[0].y < 0) {
         return true;
     }
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            return true;
+        }
+    }
 }
 function gameEngine() {
     //part1: updating the snake array and food
