f268af8d4f8c6ccba43bd89ffe705ac0d6119b3a
shuklamaneesh24 <shuklamaneesh24@gmail.com>
2024-03-16 15:25:33 +0530

    Implemented snake self-biting

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ function main(ctime) {
 function isCollide(snake) {
     // return false;
     //if you into yourself
+    for(let i = 1; i < snake.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            return true;
+        }
+    }
     
     if (snake[0].x > 18 || snake[0].x < 0 || snake[0].y > 18 || snake[0].y < 0) {
         return true;
