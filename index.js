ec7e8619fa7b7da0faccd5fa66cc8e9d8c65f1fd
anadimehta <anadimehtaa1@gmail.com>
2024-03-17 19:33:28 +0530

    The game overs when the snake bites itself

--- index.js
+++ index.js
@@ -33,9 +33,17 @@ function isCollide(snake) {
     // return false;
     //if you into yourself
     
+    for(let i=1;i<snakeArr.length;i++){
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y ){
+            return true;
+        }
+    }
+
     if (snake[0].x > 18 || snake[0].x < 0 || snake[0].y > 18 || snake[0].y < 0) {
         return true;
     }
+
+    return false;
 }
 function gameEngine() {
     //part1: updating the snake array and food
